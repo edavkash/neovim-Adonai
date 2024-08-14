dab5bd313bf85a71ef09d311a1d5aaa68e2ac5fb
David <davidkashemwa19@gmail.com>
2024-08-14 12:07:25 +0200

    new commit

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,35 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-            ensure_installed = { "vimdoc","lua", "vim", "javascript", "html", "tsx", "typescript", "python", "json" ,"powershell"},
+        configs.setup({
+            ensure_installed = {
+                "vimdoc",
+                "lua",
+                "vim",
+                "javascript",
+                "html",
+                "tsx",
+                "typescript",
+                "python",
+                "json",
+                "powershell",
+                "xml",
+                "http",
+                "graphql",
+            },
             --auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-				disable = { "help" },  -- Disable Treesitter for help files
-			},
-			indent = {
-				enable = true,
-				disable = { "help" },  -- Disable Treesitter for help files
-			},
-		})
-	end,
+            sync_install = false,
+            highlight = {
+                enable = true,
+                disable = { "help" }, -- Disable Treesitter for help files
+            },
+            indent = {
+                enable = true,
+                disable = { "help" }, -- Disable Treesitter for help files
+            },
+        })
+    end,
 }
-
