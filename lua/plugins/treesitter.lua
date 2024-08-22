090454218fbbf8945ebb52c9742d3c0dc8ce2b3b
David <davidkashemwa19@gmail.com>
2024-08-22 14:08:53 +0200

    new committe

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {  -- Specify individual parsers here
-                "vimdoc",
-                "lua",
-                "vim",
-                "javascript",
-                "html",
-                "tsx",
-                "typescript",
-                "python",
-                "json",
-                "powershell",
-                "xml",
-                "http",
-                "graphql",
-                "dockerfile",
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                disable = {},  -- List of languages to disable
-            },
-            indent = {
-                enable = true,
-                disable = {},  -- List of languages to disable
-            },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"vimdoc",
+				"lua",
+				"vim",
+				"javascript",
+				"html",
+				"tsx",
+				"typescript",
+				"python",
+				"json",
+				"powershell",
+				"xml",
+				"http",
+				"graphql",
+				"dockerfile",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = {},
+			},
+		})
+	end,
 }
-
