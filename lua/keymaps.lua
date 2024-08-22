e543a5f101f8c77b9054ed5d637af9c8a1f7d3e0
David <davidkashemwa19@gmail.com>
2024-08-22 14:20:47 +0200

    new commit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -15,16 +15,21 @@ vim.keymap.set("n", "<leader>gh", ":Gitsigns.preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>af", ':lua require("harpoon.mark").add_file()<CR>', { noremap = true, silent = true })
 
 --2. Toggling (openning/closing) harpoon UI
-vim.keymap.set("n","<leader>h",':lua require("harpoon.ui").toggle_quick_menu()<CR>',{ noremap = true, silent = true })
+vim.keymap.set(
+    "n",
+    "<leader>h",
+    ':lua require("harpoon.ui").toggle_quick_menu()<CR>',
+    { noremap = true, silent = true }
+)
 
---3.Navigate to the next bookamrk 
+--3.Navigate to the next bookamrk
 vim.keymap.set("n", "<leader>hn", ':lua require("harpoon.ui").nav_next()<CR>', { noremap = true, silent = true })
 
 --4.Navigate to the previous bookmark
 vim.keymap.set("n", "<leader>hp", ':lua require("harpoon.ui").nav_prev()<CR>', { noremap = true, silent = true })
 
 --5. keymap to open lazygit
-vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
 
 --6. mason keymap
 --show snippet functionality
@@ -37,18 +42,20 @@ vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
 vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
 
 --keymap for none_ls used for formatting code
-vim.keymap.set('n','<leader>gf',vim.lsp.buf.format,{})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
 --Keymap for telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 --Flaoterm keymap
-vim.keymap.set("n", "<leader>fn", function() vim.cmd("FloatermToggle") end)
+vim.keymap.set("n", "<leader>fn", function()
+    vim.cmd("FloatermToggle")
+end)
 
 --Moving through panes
-
-
+vim.keymap.set("n", "<space>l", "<C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<space>h", "<C-w>h", { noremap = true, silent = true })
