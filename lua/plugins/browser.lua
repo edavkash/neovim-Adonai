dd9fdfe38bde97a68691034e2450d88e483e028d
David <davidkashemwa19@gmail.com>
2024-08-23 14:38:08 +0200

    new commit

--- lua/plugins/browser.lua
+++ lua/plugins/browser.lua
@@ -1,3 +1,7 @@
-return{
-     'voldikss/vim-browser-search',
+return {
+	"voldikss/vim-browser-search",
+	config = function()
+		vim.keymap.set("n", "<space>e", ":BrowserSearch<CR>", { noremap = true, silent = true })
+		vim.keymap.set("v", "<space>e", ":BrowserSearch<CR>", { noremap = true, silent = true })
+	end,
 }
